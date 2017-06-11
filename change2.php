409e1f5f117f6174e81ddfc00d5730c7dbc2f591
Diego Prieto <diegoprieto5@gmail.com>
2017-06-11 13:45:22 +0200

    colorpicker

---
+++ change2.php
@@ -0,0 +1,10 @@
+<?php
+	$r = $_POST['slideRed'];
+	$g = $_POST['slideGreen'];
+	$b = $_POST['slideBlue'];
+
+	system("pigs p 17 $r");
+	system("pigs p 24 $g");
+	system("pigs p 22 $b");
+	header("location:colorpicker.html");
+?>
